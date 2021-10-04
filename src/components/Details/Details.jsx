6d18e1f1883e13190000ca90832deefd68991bc5
David Delgadillo <daviddel.731@gmail.com>
2021-10-04 10:05:10 -0700

    Added background image, built first components: detail component to display income and expenses and main component to keep track of expenses and later incorporate Speechly

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import { Doughnut } from 'react-chartjs-2';
+
+import useStyles from './styles.js';
+
+function Details({ title }) {
+  const classes = useStyles();
+
+  return (
+    <Card>
+      <CardHeader 
+        title={title}
+        className={title === 'Income' ? classes.income : classes.expense}
+      />
+      <CardContent>
+        <Typography variant="h5">
+          $50
+        </Typography>
+        <Doughnut data="DATA" />
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Details;
