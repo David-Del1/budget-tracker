a6f9e82dcad22ab8b8140372bfc8f4052ab029a2
David Delgadillo <daviddel.731@gmail.com>
2021-10-06 15:43:53 -0700

    Achieved basic layout of main component and started on From and List component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -18,13 +18,13 @@ function App() {
         justifyContent="center"
         style={{ height: '100vh' }}
       >
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={4}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={3}>
           <Main />
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={4}>
           <Details title="Expense" />
         </Grid>
 
--- src/components/Details/Main/Main.jsx
+++ src/components/Details/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 
 import useStyles from './styles';
+import Form from './Form/Form';
 
 function Main() {
   const classes = useStyles();
@@ -14,12 +15,13 @@ function Main() {
           Total Balance: $100
         </Typography>
         <Typography
-          variant="subtitle"
+          variant="subtitle2"
           style={{ lineHeight:'1.5em', marginTop: '20px'}}
         > 
           Try Saying: Add income for $100 in Category Salary for Monday...
         </Typography>
         <Divider />
+        <Form />
       </CardContent>
       <CardContent className={classes.cardContent}>
         <Grid container spacing={2}>
