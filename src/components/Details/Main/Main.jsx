bff742ec8d7e004b2e31042ed91eff1010eba7ba
David Delgadillo <daviddel.731@gmail.com>
2021-10-06 16:04:42 -0700

    Achieved basic layout of form and layout components

--- src/components/Details/Main/Main.jsx
+++ src/components/Details/Main/Main.jsx
@@ -3,6 +3,7 @@ import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@mater
 
 import useStyles from './styles';
 import Form from './Form/Form';
+import List from './List/List.jsx';
 
 function Main() {
   const classes = useStyles();
@@ -26,7 +27,7 @@ function Main() {
       <CardContent className={classes.cardContent}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            {/* <List /> */}
+            <List />
           </Grid>
         </Grid>
       </CardContent>
