be1666be7b44799fdb79bb7e5c4c6ff7166e0368
David Delgadillo <daviddel.731@gmail.com>
2021-10-06 17:11:44 -0700

    Integrated context to manage state

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import { Provider } from './context/context.js';
+
 import App from './App/App.jsx';
 
 ReactDOM.render(
-  <App />,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
