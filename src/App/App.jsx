93b458fe424c3a3e2af03531705f7b8cc40fa8e4
David Delgadillo <daviddel.731@gmail.com>
2021-10-07 19:21:24 -0700

    Speechly successfully integrated. Users are able to add expenses and income statements using their voice.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui';
 
 import Details from '../components/Details/Details';
 import useStyles from './styles.js';
@@ -11,24 +12,28 @@ function App() {
   return (
     <div>
       <Grid
-      className={classes.grid}
+        className={classes.grid}
         container
         spacing={0}
         alignItems="center"
         justifyContent="center"
         style={{ height: '100vh' }}
       >
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={3}>
           <Details title="Income" />
         </Grid>
         <Grid item xs={12} sm={3}>
           <Main />
         </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={3}>
           <Details title="Expense" />
         </Grid>
 
       </Grid>
+      <PushToTalkButtonContainer>
+        <PushToTalkButton />
+        <ErrorPanel />
+      </PushToTalkButtonContainer>
     </div>
   );
 }
